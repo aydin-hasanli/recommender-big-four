c21b7b9e70ad04e011b2ff1309864fdada66d476
Zi Yang <zi.yang0009@gmail.com>
2020-12-11 16:10:47 -0800

    fixed css for index1.html

--- src/flask/app.py
+++ src/flask/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 
 @app.route('/recommend', methods=['POST'])
